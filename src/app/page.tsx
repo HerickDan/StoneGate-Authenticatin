7bc19c9324c5e9cee43f44b0353a062907075899
heric <herickdan@hotmail.com>
2024-11-29 23:04:53 -0300

    background and login component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { LoginForm } from "@/pages/login/page";
 
 export default function Home() {
   return (
     <div>
-      
+        <LoginForm/>
     </div>
   );
 }
